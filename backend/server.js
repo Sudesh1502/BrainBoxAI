9bb102cede68a5713ca964ae3be7ad1696ab944e
Sudesh <sudeshmhamankar0909@gmail.com>
2025-12-08 23:50:45 +0530

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -1,55 +1,30 @@
-
-
-
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import connectDB from './configDb.js';
 import chatRoutes from './routes/chatRoutes.js';
 import userRouter from './routes/authRoutes.js';
-import cookieParser from 'cookie-parser';
-
 
 const app = express();
 
-//pase the incoming request in json body
-
-// CORS for browser + cookies
 app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://brain-box-5jyg1uu5o-sudesh-s-projects.vercel.app",
-    "https://693714f922de7dc78b0ccafa--brainboxaiweb.netlify.app/"
+    "https://693714f922de7dc78b0ccafa--brainboxaiweb.netlify.app"
   ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
-
-
-// Allow credentials header explicitly
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
-
-//useing cookie parser
+app.use(express.json());
 app.use(cookieParser());
 
-//pase the incoming request in json body
-app.use(express.json());
-
-
-//routes for api
 app.use('/api', chatRoutes);
-
-//routes for authentication
-app.use('/auth', userRouter)
-
-//test end point
-
+app.use('/auth', userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("app is listening on " + process.env.PORT);
   connectDB();
-})
+});
